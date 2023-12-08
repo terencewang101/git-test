6eb6f670a578be7ea3dfd7325c4d58228d4b4c7a
terencewang101 <149046885+terencewang101@users.noreply.github.com>
2023-12-09 00:43:27 +0800

    Add and edit JS practise files

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,19 @@
-let MAX = 57;
-let actual = MAX - 13;
-let percentage = actual / MAX;
-console.log(percentage)
+function add7(n) {
+    return n + 7;
+}
+console.log("The result of Q1 is " + add7(5) + ".")
+
+function multiply(a, b) {
+    return a * b;
+}
+console.log("The result of Q2 is " + multiply(7, 5) + ".")
+
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
+console.log("The result of Q3 is " + capitalize("teNDOn") + ".")
+
+function lastLetter(string) {
+    return string.slice(-1);
+}
+console.log("The result of Q4 is " + lastLetter("we are retrievers") + ".")
